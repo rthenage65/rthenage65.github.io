8b4462b31b073a721ad8059f5ad283b9e2f1420b
Merit Student <student@meac.local>
2019-09-11 09:56:31 -0600

    Set style to css grid

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 document.body.innerHTML = '<nav id="nav-bar"><a class="nav-link" href="index.html">HOME</a><a class="nav-link" href="games.html">GAMES</a><a class="nav-link" href="websites.html">WEBSITES</a><a class="nav-link" href="resume.html">RESUMÉ</a><div class="nav-link small"><img class="nav-img" src="tree.png"/></div></nav>' + document.body.innerHTML + '<footer>Copyright Richard Henage August 2019</footer>';
+/*
+var iframe = document.getElementById("platformer");
+
+if (iframe) {
+    iframe.onload = () => {
+        console.log(iframe.contentWindow.document);
+        var elmnt = iframe.contentWindow.document.getElementsByTagName("CANVAS")[0];
+        elmnt.style.transform = "scale(0.5)";
+    }
+}*/
